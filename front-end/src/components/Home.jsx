440fcdd4e0557fe6b9964f4dfeea1d0538376498
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-02-05 12:43:02 +0900

    [add]投稿機能作成

---
+++ front-end/src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useFormik } from "formik";
+
+export const Home = () => {
+  const initialPostValue = {
+    post: "",
+  };
+
+  const {
+    values: newPostData,
+    handleSubmit: onFinish,
+    handleChange,
+  } = useFormik({
+    initialValues: initialPostValue,
+
+    onSubmit: () => {
+      saveDate();
+    },
+  });
+
+  const saveDate = () => {
+    const data = {
+      post: newPostData.post,
+    };
+    fetch("http://localhost:8080/timeline/post", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(alert("保存完了しました。"));
+  };
+
+  return (
+    <>
+      <div>
+        <label>投稿</label>
+        <textarea
+          type="text"
+          id="post"
+          name="post"
+          value={newPostData.post}
+          onChange={handleChange}
+        />
+        <input type="submit" value="送信" onClick={() => onFinish()} />
+      </div>
+    </>
+  );
+};
